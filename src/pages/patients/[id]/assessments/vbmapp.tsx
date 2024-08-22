52d93a7cba4d13ada2bd616d4ee109f193f39fdb
Nathalia Mendonça quisbert <nathaliamendoncaquisbert@MacBook-Air-de-Nathalia.local>
2024-08-22 19:23:05 -0400

    wip

--- src/pages/patients/[id]/assessments/vbmapp.tsx
+++ src/pages/patients/[id]/assessments/vbmapp.tsx
@@ -97,7 +97,7 @@ const RadioGroupDemo = ({options} : OptionsGroupProps) => {
   )
 }
 
-export default function NewAssessment() {
+export default function VBMAppScreen() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <LoggedNavigationMenu />

866a7d6cd0016073e9dc40140ffbbe625920a430
Gabriel Loiácono <gabriel.loiacono@mercadolivre.com>
2024-08-23 19:37:59 -0400

    refactor: sticky menu and ui modifications

--- src/components/shared/navbar/logged.tsx
+++ src/components/shared/navbar/logged.tsx
@@ -39,17 +39,19 @@ import {
 export function LoggedNavigationMenu() {
   return (
     <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex h-full max-h-screen flex-col gap-2 sticky top-0">
+          <div className="flex items-center border-b px-4 py-6 lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Package2 className="h-6 w-6" />
-              <span className="">Analise Comportamental</span>
+              <Package2 className="h-6 w-6 mr-2" />
+              <span className="leading-tight">Analise Comportamental</span>
             </Link>
+
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
               <span className="sr-only">Toggle notifications</span>
             </Button>
           </div>
+
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link

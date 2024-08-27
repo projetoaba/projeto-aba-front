a31855f341f424649dae1df534a84a5256256549
Nathalia Mendonça quisbert <nathaliamendoncaquisbert@MacBook-Air-de-Nathalia.local>
2024-08-26 22:38:12 -0400

    form path

--- src/app/(dashboard)/patients/[id]/interventions/[intervention-id]/programs/new/page.tsx
+++ src/app/(dashboard)/patients/[id]/interventions/[intervention-id]/programs/new/page.tsx
@@ -35,7 +35,7 @@ const reforcements = ['Esquema de reforçamento continuo (CRF)',
 export default function NewProgramPage() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center px-8 py-4">
+      <div className="flex items-center py-4">
         <h1 className="text-lg font-semibold md:text-2xl">
           Programa de ensino
         </h1>

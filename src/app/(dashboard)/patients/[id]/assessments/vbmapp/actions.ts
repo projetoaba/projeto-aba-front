96aa6d2cddaa8e4033c8533e0f96d4fb186bde98
Ana Wivian <rodrigoanaprogramador@gmail.com>
2024-09-17 15:12:09 -0300

    Up
    Método salvar

--- src/app/(dashboard)/patients/[id]/assessments/vbmapp/actions.ts
+++ src/app/(dashboard)/patients/[id]/assessments/vbmapp/actions.ts
@@ -1,28 +1,17 @@
-"use server";
-import * as v from "valibot";
-import { VbmappQuestion } from "./components/questions";
-import { errorTransforms } from "@/lib/transforms/error";
+import { api } from "@/lib/api";
 
-const VbmappQuestionsSchema = v.object(
-  v.entriesFromList(
-    Object.values(VbmappQuestion),
-    v.string("Você deve responder todas as perguntas.")
-  )
-);
-
-export type VbmappQuestions = v.InferOutput<typeof VbmappQuestionsSchema>;
 
 export const saveVbmappQuestions = async (
   previousState: any,
   formData: FormData
 ) => {
+  const respostas = formData.get('answers');
+  const request = { patient_id: '', answers: respostas };
+
   try {
-    const questionResults = Object.fromEntries(formData);
-
-    return { data: v.parse(VbmappQuestionsSchema, questionResults) };
-  } catch (error: unknown) {
-    console.log(error);
-
-    return errorTransforms.parseToObject(error);
+    const response = await api().post('api/application-assessments', request);
+    console.log("Dados enviados com sucesso:", response.data);
+  } catch (error) {
+    console.error("Erro ao enviar dados:", error);
   }
 };

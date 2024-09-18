d45333334527e31e3cfb021935454740fd0d6322
Nathalia Mendonça quisbert <nathaliamendoncaquisbert@MacBook-Air-de-Nathalia.local>
2024-09-18 09:54:51 -0400

    salvando

--- src/components/shared/table/assessments.tsx
+++ src/components/shared/table/assessments.tsx
@@ -3,16 +3,9 @@
 import * as React from "react"
 import {
   ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
 } from "@tanstack/react-table"
+import { useEffect, useState } from "react"
+import Link from "next/link"
 import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
 
 import { Button } from "@/components/shared/ui/button"
@@ -33,30 +26,29 @@ import {
   TableCell,
   TableHead,
   TableHeader,
+  TableFooter,
   TableRow,
 } from "@/components/shared/ui/table"
+import { api } from '@/lib/api'
 
 const data: Assesment[] = [
   {
     id: "m5gr84i9",
-    createdAt: "2024-08-12 13:00:01",
-    type: "vb-mapp",
-    status: "success",
+    created_at: "2024-08-12 13:00:01",
+    type: "vb-mapp",    
   },
   {
     id: "3u1reuv4",
-    createdAt: "2024-08-12 13:00:01",
+    created_at: "2024-08-12 13:00:01",
     type: "vb-mapp",
-    status: "success",
   },
 ]
 
 export type Assesment = {
   id: string
-  status: "pending" | "processing" | "success" | "failed"
   type: string
-  createdAt: string
-  completedAt?: string
+  created_at: string
+  completed_at?: string
 }
 
 export const columns: ColumnDef<Assesment>[] = [
@@ -143,108 +135,68 @@ export const columns: ColumnDef<Assesment>[] = [
 ]
 
 export default function AssessmentsTable() {
-  const [sorting, setSorting] = React.useState<SortingState>([])
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  )
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({})
-  const [rowSelection, setRowSelection] = React.useState({})
-
-  const table = useReactTable({
-    data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
-  })
+  const [patients, setPatients] = useState<Assesment[]>([])
+  useEffect(() => {
+    api().get('/api/patients')
+      .then(function (response) {
+        console.log(response.data);
+        const data = response.data.data
+        setPatients(data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <div className="w-full px-6">      
-      <div className="rounded-md border">
+    <div className="w-full px-6">
+      <div className="flex items-center px-0 py-4">
         <Table>
           <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
+            <TableRow>
+              <TableHead className="w-[100px]">Protocolo</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Data de Nascimento</TableHead>
+              <TableHead className="text-right">...</TableHead>
+            </TableRow>
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
+            {patients.map((patient) => (
+              <TableRow key={patient.id}>
+                <TableCell className="font-medium">VBMAPP</TableCell>
+                <TableCell>{patient.created_at}</TableCell>
+                <TableCell>{patient.completed_at}</TableCell>
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                      <DropdownMenuItem
+                        onClick={() => navigator.clipboard.writeText(patient.id)}
+                      >
+                        Copiar paciente
+                      </DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem><Link href="patients/12/assessments">Avaliaçoes</Link></DropdownMenuItem>
+                      <DropdownMenuItem><Link href="patients/12/interventions">Programas</Link> </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </TableCell>
               </TableRow>
-            )}
+            ))}
           </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">$2,500.00</TableCell>
+            </TableRow>
+          </TableFooter>
         </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
       </div>
     </div>
   )

de0738db25a721f4585bc5c93663a80721d7cefa
Nathalia Mendonça quisbert <nathaliamendoncaquisbert@MacBook-Air-de-Nathalia.local>
2024-09-18 09:46:44 -0400

    salvando

--- src/app/(dashboard)/patients/[id]/assessments/vbmapp/actions.ts
+++ src/app/(dashboard)/patients/[id]/assessments/vbmapp/actions.ts
@@ -1,15 +1,18 @@
 import { api } from "@/lib/api";
 
-
 export const saveVbmappQuestions = async (
   previousState: any,
   formData: FormData
 ) => {
-  const respostas = formData.get('answers');
-  const request = { patient_id: '', answers: respostas };
+  const questionResults = Object.fromEntries(formData);
+  const patient_id = questionResults.patient_id
+  delete questionResults['patient_id']
+
+  const request = { patient_id: patient_id, answers: questionResults };
+  console.log(request)
 
   try {
-    const response = await api().post('api/application-assessments', request);
+    const response = await api().post('api/assessments-applications', request);
     console.log("Dados enviados com sucesso:", response.data);
   } catch (error) {
     console.error("Erro ao enviar dados:", error);

db75170a2fc5edd31cb9c4fede8f2fa7edb765a6
projetoaba <projeto.aba.eng@gmail.com>
2024-09-20 09:42:27 -0400

    Update page.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,13 +3,6 @@ import Link from "next/link"
 
 import { LoggedNavigationMenu, LoggedNavbar } from '@/components/shared/navbar/logged'
 
-export const description =
-  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
-
-export const iframeHeight = "800px"
-
-export const containerClassName = "w-full h-full"
-
 export default function Dashboard() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">

1b83575fd755a6532d80fcdc55dab3278be317b6
projetoaba <projeto.aba.eng@gmail.com>
2024-09-20 13:23:48 -0400

    Update page.tsx

--- src/app/(dashboard)/tools/page.tsx
+++ src/app/(dashboard)/tools/page.tsx
@@ -1,4 +1,4 @@
-export default async function PatientsScreen() {
+export default function ToolsScreen() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">        
     </main>

6cd75b5e1a619a724213508f35477eb237421990
Nathalia Mendonça quisbert <nathaliamendoncaquisbert@MacBook-Air-de-Nathalia.local>
2024-10-17 00:39:22 -0400

    chart fixs

--- src/app/(dashboard)/patients/[id]/assessments/[assessment_id]/simple-chart/page.tsx
+++ src/app/(dashboard)/patients/[id]/assessments/[assessment_id]/simple-chart/page.tsx
@@ -69,14 +69,13 @@ export default function PatientsScreen() {
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-2xl">Resultados</h1>            
-          </div>          
+          </div>
+          <SummaryHeader patient_id={patient_id}/>
           <div
             className="flex flex-1 w-full justify-center rounded-lg border border-dashed shadow-sm py-8"
           >      
-          <div className='grid gap-2 grid-cols-1 w-full'>
-          <SummaryHeader patient_id={patient_id}/>
             <LineChart dataset={assessment}/>
-          </div>            
+            {/* </div> */}
           </div>
     </main>
   )
